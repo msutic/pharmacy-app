63bc8342b5245abd3b459d56dd89669df145b4c9
msutic <msuticm@gmail.com>
2024-10-27 20:31:51 +0100

    Random bar colors

--- src/views/statistics/ProductPriceChart.tsx
+++ src/views/statistics/ProductPriceChart.tsx
@@ -7,6 +7,7 @@ import {
   Tooltip,
   CartesianGrid,
   Legend,
+  Cell,
 } from 'recharts';
 import { Product } from '../products/types';
 
@@ -23,10 +24,23 @@ const ProductPriceChart: React.FC<{ products: Product[] }> = ({ products }) => {
     })
   );
 
+  const colors = [
+    '#8884d8',
+    '#82ca9d',
+    '#ffc658',
+    '#ff7300',
+    '#d0ed57',
+    '#a4de6c',
+    '#8dd1e1',
+    '#ffb3e6',
+    '#ff6666',
+    '#66b3ff',
+  ];
+
   return (
     <BarChart
-      width={700}
-      height={300}
+      width={1000}
+      height={400}
       data={chartData}
       margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
     >
@@ -35,7 +49,11 @@ const ProductPriceChart: React.FC<{ products: Product[] }> = ({ products }) => {
       <YAxis />
       <Tooltip />
       <Legend />
-      <Bar dataKey="price" barSize={40} fill="#8884d8" />
+      <Bar dataKey="price" barSize={40}>
+        {chartData.map((_entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+        ))}
+      </Bar>
     </BarChart>
   );
 };
