a6d3af29b513f41ac1351d1010a37557a8c33eeb
msutic <msuticm@gmail.com>
2024-10-27 22:09:05 +0100

    Update about page

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -1,3 +1,13 @@
-const About = () => <h1>About</h1>;
+import './About.scss';
+
+const About: React.FC = () => {
+  return (
+    <div className="about-container">
+      <h1>Pharmacy App</h1>
+      <p>Version: 1.0.0</p>
+      <p>Created by: Marko Sutic</p>
+    </div>
+  );
+};
 
 export default About;
